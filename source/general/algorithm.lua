25275f502b646f074a657e57f40d7172f4bdbd69
Lazy Knight <i@lazyknight.com>
2024-12-13 09:12:07 +0800

    doc: model_radius_transform

--- source/general/algorithm.lua
+++ source/general/algorithm.lua
@@ -1,4 +1,5 @@
 ---Calculate model size for specifici radius.
+---<br>基于标准模型尺寸和模型半径，计算目标半径下的模型尺寸。
 ---@param model_standard_radius number
 ---@param model_standard_size number
 ---@param target_radius number
